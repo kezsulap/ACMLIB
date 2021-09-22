caac2b68f2586d596ceba649b1424330a0d17a74
Konrad Paluszek <kezsulap@gmail.com>
2021-09-23 00:01:10 +0200

    Fixed Compression

--- code/Miscellaneous/Compress.cpp
+++ code/Miscellaneous/Compress.cpp
@@ -1,24 +1,30 @@
-//TODO: use plain-text escaping only necessary characters '\n', '\r', '\\', '\"'
-struct Compress {//7 na 8 bitów używa
+struct Compress {//Uses all 8 bits
 	static unsigned char data[];
-	int pos = -1, left = 1;
+	int pos = 0, curr = -1;
 	uint64_t Data(int bits, uint64_t x = 0) {
 		assert(1 <= bits and bits <= 64);
 		for (int i = 0; i < bits; i++) {
-			if (left-- == 1) { pos++; left = 7; }
-			if (((x >> i) ^ (data[pos] >> left)) & 1) {
-				data[pos] ^= 1 << left;
+			if (++curr == 8) { pos++; curr = 0; }
+			if (((x >> i) ^ (data[pos] >> curr)) & 1) {
+				data[pos] ^= 1 << curr;
 				x ^= 1llu << i;
 			}
 		}
 		return x;
 	}
 	void Print() {
-		constexpr char code[] = "KaMylMaDoWnA";  // Length must be <= 16.
-		std::ofstream out("compress.cpp");
-		out << "unsigned char Compress::data[] = R\"" << code << "(";
-		out.write(reinterpret_cast<const char*>(data), pos + 1);
-		out << ")" << code << "\";\n";
+		FILE *fp = fopen("compressed.cpp", "w");
+		fprintf(fp, "unsigned char Compress::data[] = \"");
+		for (int i = 0; i <= pos; ++i) {
+			unsigned char x = data[i];
+			if (x == '\n') fprintf(fp, "\\n");
+			else if (x == '\r') fprintf(fp, "\\r");
+			else if (x == '\\' || x == '\"') fprintf(fp, "\\%c", x);
+			// else if (x == '\?') fprintf(fp, "\\%c", x); //required only if online judge still uses trigraphs
+			else putc(x, fp);
+		}
+		fprintf(fp, "\";\n");
+		fclose(fp);
 	}
 };
 unsigned char Compress::data[2 * 128 * 1024 /* More than will be written. */];
