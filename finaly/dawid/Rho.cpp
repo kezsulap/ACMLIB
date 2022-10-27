436fb4627dcc04707fd517eae6f50ffd2300e205
shadowatyy <shadowaty97@gmail.com>
2022-10-27 22:48:40 +0200

    rho

--- finaly/dawid/Rho.cpp
+++ finaly/dawid/Rho.cpp
@@ -6,32 +6,32 @@ int ptot;
 int d[maxp];
 int pr[maxp];
 vector<ll> ans;  // rozklad, czyscimy zazwyczaj po kazdym wywolaniu decompose()
-inline LL mod(LL a, LL n) {
+inline ll mod(ll a, ll n) {
   if (a >= n) a -= n;
   return a;
 }
-inline LL add(LL a, LL b, LL n) {
+inline ll add(ll a, ll b, ll n) {
   a += b;
   mod(a, n);
   return a;
 }
-inline LL mul(LL x, LL y, LL p) { // uwaga, ta funkcja rzuca overflow, tak ma byc
-  LL ret = x * y - (LL)((long double)x * y / p + 0.5) * p;
+inline ll mul(ll x, ll y, ll p) { // uwaga, ta funkcja rzuca overflow, tak ma byc
+  ll ret = x * y - (ll)((ld)x * y / p + 0.5) * p;
   return ret < 0 ? ret + p : ret;
 }
-LL fast(LL x, LL k, LL p) {
-  LL ret = 1 % p;
+ll fast(ll x, ll k, ll p) {
+  ll ret = 1 % p;
   for (; k > 0; k >>= 1, x = mul(x, x, p)) (k & 1) && (ret = mul(ret, x, p));
   return ret;
 }
-bool rabin(LL n) {
+bool rabin(ll n) {
   if (n == 2) return 1;
   if (n < 2 || !(n & 1)) return 0;
-  LL s = 0, r = n - 1;
+  ll s = 0, r = n - 1;
   for (; !(r & 1); r >>= 1, ++s)
     ;
   for (int i = 0; pr[i] < n && pr[i] < maxv; ++i) {
-    LL cur = fast(pr[i], r, n), nxt;
+    ll cur = fast(pr[i], r, n), nxt;
     for (int j = 0; j < s; ++j) {
       nxt = mul(cur, cur, n);
       if (nxt == 1 && cur != 1 && cur != n - 1) return false;
@@ -41,27 +41,17 @@ bool rabin(LL n) {
   }
   return true;
 }
-LL gcd(LL a, LL b) { // odrobine szybsze niz wbudowane, mozna skipnac
-  LL tmp;
-  while (b) {
-    tmp = b;
-    b = a % b;
-    a = tmp;
-  }
-
-  return a;
-}
-LL factor(LL n) {
-  static LL seq[maxp];
+ll factor(ll n) {
+  static ll seq[maxp];
   while (true) {
-    LL x = rand() % n, y = x, c = rand() % n;
-    LL *px = seq, *py = seq, tim = 0, prd = 1;
+    ll x = rand() % n, y = x, c = rand() % n;
+    ll *px = seq, *py = seq, tim = 0, prd = 1;
 
     while (true) {
       *py++ = y = add(mul(y, y, n), c, n);
       *py++ = y = add(mul(y, y, n), c, n);
       if ((x = *px++) == y) break;
-      LL tmp = prd;
+      ll tmp = prd;
       prd = mul(prd, abs(y - x), n);
       if (!prd) return gcd(tmp, n);
       if ((++tim) == maxv) {
@@ -72,13 +62,13 @@ LL factor(LL n) {
     if (tim && (prd = gcd(prd, n)) > 1 && prd < n) return prd;
   }
 }
-void decompose(LL n) {  // glowna funkcja
+void decompose(ll n) {  // glowna funkcja
   if (n < maxp) {
     while (n > 1) ans.pb(d[n]), n /= d[n];
   } else if (rabin(n))
     ans.pb(n);
   else {
-    LL fact = factor(n);
+    ll fact = factor(n);
     decompose(fact), decompose(n / fact);
   }
 }
